0564bad87d0d031a494d30a42f18e437b10010a0
Alv-Rod <117731512+Alv-Rod@users.noreply.github.com>
2022-11-28 09:30:05 -0300

    Add files via upload

---
+++ #algoritmo senha.py
@@ -0,0 +1,12 @@
+#algoritmo senha
+#alvaro
+import random
+senhaCorreta = random.randint(0, 10)
+senhaUsuario = 0
+print(senhaCorreta)
+while senhaUsuario != senhaCorreta:
+    senhaUsuario = int(input("Insira sua senha... "))
+    if senhaUsuario != senhaCorreta:
+        print("senha incorreta, tente novamente... ")
+print("senha correta.")
+print("fim")
